186c378e8f1e0ebde17684fc0f04843d238f92cb
bummay <37524444+bummay@users.noreply.github.com>
2019-12-03 15:12:25 +0900

    Haloween Saleを修正（クリア）

--- ProblemSolving/HaloweenSale.py
+++ ProblemSolving/HaloweenSale.py
@@ -16,22 +16,29 @@ def howManyGames(p, d, m, s):
         ret += 1
         balance -= p
 
-    cutted = p
-    while cutted > m:
-        cutted -= d
-        if cutted < m:
-            break
-        if balance - cutted > 0:
-            ret += 1
-            balance -= cutted
-
-    ret += balance // m
+        cutted = p
+        while cutted >= m:
+            cutted -= d
+            if cutted < m:
+                ret += balance // m
+                break
+            if balance - cutted > 0:
+                ret += 1
+                balance -= cutted
+            else:
+                break
 
     return ret
 
 
 if __name__ == '__main__':
     inputs = [
+                [1, 100, 1, 9777],
+                [100, 1, 1, 99],
+                [100, 19, 1, 180],
+                [100, 12, 55, 95],
+                [80, 60, 27, 7777],
+                [80, 43, 2, 7602],
                 [20, 3, 6, 80],
                 [20, 3, 6, 85]
     ]
