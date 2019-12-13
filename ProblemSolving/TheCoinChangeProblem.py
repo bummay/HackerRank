0626bef10e7262051e7108705b0486ca85cd599e
bummay <37524444+bummay@users.noreply.github.com>
2019-12-13 11:42:39 +0900

    Solved!!

--- ProblemSolving/TheCoinChangeProblem.py
+++ ProblemSolving/TheCoinChangeProblem.py
@@ -6,14 +6,7 @@ import sys
 
 from collections import defaultdict
 
-
 def splitCoins(val, coins, coinList, ret, dict):
-    # change <= coin の場合
-    #   計算済みパターン > 0 ならdictからパターン数を取得
-    #   計算済みパターン = 0 なら以降の再回帰でパターン数を取得しdictに保存
-    # change > coin の場合
-    # 再回帰でパターン数を取得。ただし結果はdictに保存しない。
-    # change in coins の場合は再回帰処理が終わった最後にretとdictに保存
 
     coins.sort(reverse = True)
     if len(coinList) > 0:
@@ -25,63 +18,72 @@ def splitCoins(val, coins, coinList, ret, dict):
 
     splitted = [i for i in coins if i <= limit and val - i >= min(coins)]
     for coin in splitted:
-        if coin == 1:
+        if coin == min(coins) and val % coin == 0:
             appended = sorted(coinList + [coin] * (val // coin))
-            # print(appended)
             ret += 1
             break
+
         isSet = False
         change = val - coin
-        coinList += [coin]
-        if dict[change] < 0:
-            coinList.pop(-1)
-            # break
-        elif change <= coin:
-            if dict[change] > 0 :
-                ret += dict[change]
-                coinList.pop(-1)
-            else:
-                isGetPattern = True
-                if change in coins:
-                    dict[change] += 1
+        dictChange = dict[change]
+        dictCoinChange = dict[(coin,change)]
 
-                if change <= coin and change in coins:
-                    appended = sorted(coinList + [change])
-                    # print(appended)
-                    ret += 1
-                dict[change] += splitCoins(change, coins, coinList, 0, dict)
-                a = coinList.pop(-1)
-                dict[val] += dict[change]
-                if dict[change] == 0:
-                    dict[change] = -1
-
+        if len([i for i in coins if i < change]) > 0:
+            maxCoin = max([i for i in coins if i <= change])
         else:
-            rec = splitCoins(change,coins,coinList, 0, dict)
-            a = coinList.pop(-1)
-            if rec == 0:
-                dict[change] = -1
+            maxCoin = 0
+        isGreqThanMaxCoin = (coin >= maxCoin and maxCoin != min(coins))
+
+        if isGreqThanMaxCoin and dictChange > 0:
+            ret += dictChange
+
+        elif isGreqThanMaxCoin != True and dictCoinChange > 0:
+            ret += dict[(coin, change)]
+
+        elif dictChange >= 0 and dict[(coin, change)] >= 0:
+            result = 0
+            coinList += [coin]
+            if dictChange == 0:
+                if isGreqThanMaxCoin:
+                    isSet = True
+                if isSet and change in coins:
+                    result += 1
+            result += splitCoins(change, coins, coinList, 0, dict)
+
+            coinList.pop(-1)
+            ret += result
+            if isSet:
+                if result == 0:
+                    dict[change] = -1
+                else:
+                    dict[change] = result
             else:
-                ret += rec
+                if result == 0:
+                    dict[(coin, change)] = -1
+                else:
+                    dict[(coin, change)] = result
     return ret
 
 def getWays(n, c):
     ret = 0
     c.sort(reverse = True)
     stock = defaultdict(int)
-    dictIsSet = defaultdict(bool)
     return splitCoins(n, c, [], ret, stock)
 
 
 if __name__ == '__main__':
     inputs = [
-        [75, [25, 10, 11, 29, 49, 31, 33, 39, 12, 36, 40, 22, 21, 16, 37, 8, 18, 4, 27, 17, 26, 32, 6, 38, 2, 30, 34], 16694],
+        [10, [1,2,3,5,6], 24],
+
         [18, [49, 9, 40, 17, 46, 24, 42, 26, 43, 41, 35, 1, 47, 28, 20, 38, 2, 44, 32, 22, 18, 45, 25], 18],
+        [75, [25, 10, 11, 29, 49, 31, 33, 39, 12, 36, 40, 22, 21, 16, 37, 8, 18, 4, 27, 17, 26, 32, 6, 38, 2, 30, 34], 16694],
         [20, [1,2,5,7,8,10,11,12],103],
-        [2, [44, 5, 9, 39, 6, 25, 3, 28, 16, 19, 4, 49, 40, 22, 2, 12, 45, 33, 23, 42, 34, 15, 46, 26, 13, 31, 8], 1],
-        [4, [1, 2, 3], 4],
         [10, [2, 5, 3, 6], 5],
         [15, [49, 22, 45, 6, 11, 20, 30, 10, 46, 8, 32, 48, 2, 41, 43, 5, 39, 16, 28, 44, 14, 4, 27, 36], 10],
-        [10, [1,2,3,5,6], 24],
+        [4, [1, 2, 3], 4],
+
+
+        [2, [44, 5, 9, 39, 6, 25, 3, 28, 16, 19, 4, 49, 40, 22, 2, 12, 45, 33, 23, 42, 34, 15, 46, 26, 13, 31, 8], 1],
         [69, [25, 27, 40, 38, 17, 2, 28, 23, 9, 43, 18, 49, 15, 24, 19, 11, 1, 39, 32, 16, 35, 30, 48, 34, 20, 3, 6, 13, 44], 101768],
         [166, [5, 37, 8, 39, 33, 17, 22, 32, 13, 7, 10, 35, 40, 2, 43, 49, 46, 19, 41, 1, 12, 11, 28], 96190959],
         [219, [36, 10, 42, 7, 50, 1, 49, 24, 37, 12, 34, 13, 39, 18, 8, 29, 19, 43, 5, 44, 28, 23, 35, 26], 168312708],
@@ -92,4 +94,4 @@ if __name__ == '__main__':
     for i in range(len(inputs)):
         result = getWays(inputs[i][0], inputs[i][1])
         isTrue = (result == inputs[i][2])
-        print(str(i) + ":" + str(result) + ":" + str(isTrue))
+        print(str(i) + ":" + str(inputs[i][0]) + ":" + str(result) + ":" + str(isTrue))
